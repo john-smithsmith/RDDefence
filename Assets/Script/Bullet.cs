0a0376b16723872358632689c71605bf3b69de6e
조원후 <purplehaze0@naver.com>
2025-12-26 23:42:00 +0900

    [Test] Bullet ChainDamage
    Bullet ChainDamage

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class Bullet : MonoBehaviour
 {
@@ -6,6 +7,10 @@ public class Bullet : MonoBehaviour
     private int damage;
     private DiceType type;
     private float speed = 10f;
+
+    [Header("Lightning Settings")]
+    private int maxTargets = 3;    
+    private float chainRange = 4.0f;
 
     public void Init(Transform _target, int _damage, DiceType _type)
     {
@@ -37,12 +42,61 @@ public class Bullet : MonoBehaviour
             PoolManager.Instance.ReturnToPool(gameObject);
         }
     }
-    void ApplyDamageEffect(Enemy Target)
+    void ApplyDamageEffect(Enemy primaryTarget)
     {
-        Target.TakeDamage(damage);
+        primaryTarget.TakeDamage(damage);
+
         if (type == DiceType.Ice)
         {
-            Target.ApplySlow(0.7f); // 30% °¨¼Ó
+            primaryTarget.ApplySlow(0.7f);
+        }
+        else if (type == DiceType.Lightning)
+        {
+            HashSet<int> visitedEnemies = new HashSet<int>();
+            visitedEnemies.Add(primaryTarget.gameObject.GetInstanceID());
+            ChainDamage(primaryTarget.transform.position, maxTargets - 1, visitedEnemies);
+        }
+    }
+
+    void ChainDamage(Vector3 startPos, int remainingCount, HashSet<int> visited)
+    {
+        if (remainingCount <= 0) return;
+
+        Collider2D[] hits = Physics2D.OverlapCircleAll(startPos, chainRange);
+
+        Enemy nextTarget = null;
+        float minDistance = float.MaxValue;
+
+        foreach (Collider2D hit in hits)
+        {
+            if (hit.CompareTag("Enemy"))
+            {
+                int id = hit.gameObject.GetInstanceID();
+                if (visited.Contains(id)) continue;
+
+                Enemy enemyScript = hit.GetComponent<Enemy>();
+                if (enemyScript != null && enemyScript.gameObject.activeInHierarchy)
+                {
+                    float dist = Vector3.Distance(startPos, hit.transform.position);
+
+                    if (dist < minDistance)
+                    {
+                        minDistance = dist;
+                        nextTarget = enemyScript;
+                    }
+                }
+            }
+        }
+
+        if (nextTarget != null)
+        {
+            nextTarget.TakeDamage(damage);
+
+            visited.Add(nextTarget.gameObject.GetInstanceID());
+
+            Debug.DrawLine(startPos, nextTarget.transform.position, Color.yellow, 0.2f);
+
+            ChainDamage(nextTarget.transform.position, remainingCount - 1, visited);
         }
     }
 }
